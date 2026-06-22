9cf8119814a068b944c75e377465abd6fbc72bf6
VertiaevBogdan <vertyaev95@gmail.com>
2026-06-22 22:40:06 +0200

    created table with attributes

---
+++ backend/src/main/java/com/vert/backend/model/entity/Task.java
@@ -0,0 +1,56 @@
+package com.vert.backend.model.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "TASKS")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "TASK_TITLE", length = 100)
+    private String title;
+
+    @Column(name = "TEXT", nullable = false)
+    private String text;
+
+    @Column(name = "STATUS", nullable = false)
+    private Boolean status = false;
+
+    public Task() {}
+
+    public Task(String taskTitle, String text, Boolean status){
+        this.title = taskTitle;
+        this.text = text;
+        this.status = status;
+    }
+
+    public long getId(){
+        return id;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getText(){
+        return text;
+    }
+
+    public Boolean getStatus(){
+        return status;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public void setText(String text){
+        this.text = text;
+    }
+
+    public void setStatus(Boolean status){
+        this.status = status;
+    }
+}
