8ca9fabe89276ae09663a0f6ddce8e33949f8ce9
VertiaevBogdan <vertyaev95@gmail.com>
2026-07-06 19:39:39 +0200

    added min height to TaskList

--- frontend/src/components/TasksList.tsx
+++ frontend/src/components/TasksList.tsx
@@ -9,7 +9,7 @@ export default function TasksList({ tasks, onDelete} : Props){
     return (
         <section>
             <span className="w-full p-4 pb-2 text-l opacity-60 tracking-wide">All tasks</span>
-            <ul className="bg-base-200 rounded-box shadow-md grid grid-cols-[repeat(auto-fill,minmax(24rem,1fr))] gap-4 p-4">
+            <ul className="min-h-[40em] bg-base-200 rounded-box shadow-md grid grid-cols-[repeat(auto-fill,minmax(24rem,1fr))] gap-4 p-4">
                 {tasks.map(task => (
                     <TaskCard
                         task={task}
